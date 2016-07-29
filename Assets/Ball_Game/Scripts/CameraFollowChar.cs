95010c63a8be58dd088a4060c611c0bb583a3a60
France Jaamann <FranceJaamann@gmail.com>
2016-07-29 21:11:41 +0300

    commit

---
+++ Assets/Ball_Game/Scripts/CameraFollowChar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowChar : MonoBehaviour {
+	public Transform player;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+	}
+}
---
+++ Assets/Ball_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.D))
+			transform.position  += new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+		if (Input.GetKey (KeyCode.A))
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);	
+	}		
+}
